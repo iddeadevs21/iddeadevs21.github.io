03004fc6244a0c9fb67d8dc8a661419814515918
Branko96 <branko-96@hotmail.com>
2021-09-22 21:16:37 -0300

    Mail send contact

---
+++ js/mail.js
@@ -0,0 +1,28 @@
+function sendEmail(name, email, phone, message) {
+	Email.send({
+        Host: "smtp.gmail.com",
+        Username : "iddeadevs.services@gmail.com",
+        Password : "Cartagena7081",
+        To : 'iddeadevs.services@gmail.com',
+        From : email,
+        Subject : "Iddeadevs web contact",
+        Body : name + " " + phone + " " + message,
+	}).then(
+		message => alert("Mensaje enviado!")
+	);
+}
+
+var submit_contact_form = function(event) {
+    event.preventDefault();
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phoneNumber").value;
+    var message = document.getElementById("message").value;
+    sendEmail(name, email, phone, message);
+};
+
+// your form
+var form = document.getElementById("contact_form");
+
+// attach event listener
+form.addEventListener("submit", submit_contact_form, true);
